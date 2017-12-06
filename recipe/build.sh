dad1f11e5ba5b29f3084f0bb648d7c8e7bfd57da
Jonathan Helmus <jjhelmus@gmail.com>
2017-12-06 10:09:09 -0600

    update recipe for use with conda build 3
    * update to version 3.4
    * use compiler packages
    * disable windows build for time being
    * LGPL build

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,16 +7,18 @@ unset SUBDIR
         --prefix="${PREFIX}" \
         --disable-doc \
         --enable-shared \
-        --enable-static \
-        --extra-cflags="-Wall -g -m64 -pipe -O3 -march=x86-64 -fPIC `pkg-config --cflags zlib`" \
-        --extra-cxxflags=="-Wall -g -m64 -pipe -O3 -march=x86-64 -fPIC" \
+        --extra-cflags="-fPIC `pkg-config --cflags zlib`" \
+        --extra-cxxflags=="-fPIC" \
         --extra-libs="`pkg-config --libs zlib`" \
         --enable-pic \
-        --enable-gpl \
-        --enable-version3 \
-        --enable-hardcoded-tables \
-        --enable-avresample \
-        --enable-libx264
+        --disable-static \
+        --disable-gpl \
+        --disable-nonfree \
+        --disable-openssl \
+        --enable-libvpx \
+        --cc=${CC}      \
+        --cxx=${CXX}      \
+        --enable-libopus
 
-make
+make -j${CPU_COUNT}
 make install
